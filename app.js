210df6f4071a06a7a47b6d80ec98cba13b78a279
Sungguk Lim <sungguk.lim@sap.com>
2018-02-05 11:52:25 +0900

    Add root(/) redirect

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ var port = process.env.PORT || 1337;
  
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
- 
+
+app.get('/', function(req, res) {
+  res.redirect('https://hdbgerrit.wdf.sap.corp:8443/');
+});
+
 app.use(function(req, res, next) {
   res.redirect('https://hdbgerrit.wdf.sap.corp:8443/#/c/' + req.path.slice(1));
 });
- 
+
 app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
